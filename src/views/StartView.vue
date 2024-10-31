7b91c0232657c9b59a516959075cf5744eee4c9b
petrznam <petrznam1234@gmail.com>
2024-10-31 21:18:34 +0300

    создал кнопку

---
+++ src/views/StartView.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Стартовый экран викторины</h1>
+    <h3>введите своё имя</h3>
+    <input class="name-input" type="text" v-model="name">
+    <button>Найти соперника</button>
+</template>
+<script>
+export default{
+ data(){
+    return{
+        name: ""
+    }
+ }
+}
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+.name-input{
+    width: 500px;
+    height: 50px;
+    font-family: "Rubik", sans-serif;
+}
+</style>
